8994ee9579566c55d03607eb5e8e547afaa4e3ca
Henrikilis <73853874+Henrikilis@users.noreply.github.com>
2020-11-19 10:57:29 -0300

    barradehpesimplepool

---
+++ healthBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image foregroundImage;
+    [SerializeField]
+    private float updateSpeedSeconds = 0.5f;
+
+    private void Awake()
+    {
+        GetComponentInParent<hpbar>().OnHealthPercentage += HandleHealthChange;
+    }
+
+    private void HandleHealthChange(float value)
+    {
+        StartCoroutine(changeToPct(value));
+
+    }
+
+    private IEnumerator changeToPct(float pct)
+    {
+        float preChangePct = foregroundImage.fillAmount;
+        float elapsed = 0f;
+
+        while (elapsed <updateSpeedSeconds)
+        {
+            elapsed += Time.deltaTime;
+            foregroundImage.fillAmount = Mathf.Lerp(preChangePct, pct, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+        foregroundImage.fillAmount = pct;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
---
+++ hpbar.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class hpbar : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 20;
+
+    private int currentHealth;
+    public bool town = false;
+    public event Action<float> OnHealthPercentage = delegate { };
+    
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void modifyHealth(int amount)
+    {
+        currentHealth += amount;
+
+        float currentHealthpct = (float)currentHealth / (float)maxHealth;
+
+        OnHealthPercentage(currentHealthpct);
+
+    }
+
+    public void decreaseHealth(int amount)
+    {
+        currentHealth -= amount;
+
+        float currentHealthpct = (float)currentHealth / (float)maxHealth;
+
+        OnHealthPercentage(currentHealthpct);
+
+    }
+
+
+    void Update()
+    {
+        if(currentHealth <= 0)
+        {
+            if (town)
+            {
+                GameEvents.current.GameOver("Lose");
+            }
+            GameEvents.current.DestroyTower(this.gameObject);
+            Destroy(this.gameObject, 0.1f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "inimigoBloco")
+        {
+            Destroy(collision.gameObject);
+            decreaseHealth(1);
+        }
+    }
+}
